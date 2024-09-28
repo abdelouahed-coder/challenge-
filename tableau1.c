76f720a3d688c98f8a2ca77b2531c0fdb94a88fa
unknown <abdelouahed.chahyd0@gmail.com>
2024-09-28 16:03:56 +0200

    add tbleau

---
+++ tableau1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+   int t[] = {1,2,3,4,5};
+
+      for(int i = 0 ; i < 5 ; i++){
+          printf("%d\n",t[i]);
+      }
+
+
+    return 0;
+}
