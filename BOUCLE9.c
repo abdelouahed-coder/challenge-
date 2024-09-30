ed0e8f860ba1b864b87263c15983b1c895751e27
unknown <abdelouahed.chahyd0@gmail.com>
2024-09-30 12:58:38 +0200

    add file

---
+++ BOUCLE9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+int n,i,z=0;
+ printf("entrer un nombre");
+  scanf("%d", &n);
+  for(i = 0; i <= n; i++){
+      z = z+i;
+  }
+      printf("la somme est :%d\n",z);
+
+
+
+  return 0;
+}
