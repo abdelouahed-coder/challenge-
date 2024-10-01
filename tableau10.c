14ed59c951be5f3d2a14db7c9c6ea8d0b35470dd
unknown <abdelouahed.chahyd0@gmail.com>
2024-10-01 11:36:37 +0100

    add file

--- tableau10.c
+++ tableau10.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 int main(){
-int n,y;
- printf("entrer la taille :");
-  scanf("%d", &n);
+
+ int n,y;
+   printf("entrer la taille :");
+    scanf("%d", &n);
+
  int T[n];
-  printf("entrer les element\n");
-   for(int i = 0; i < n; i++){
-       scanf("%d",&T[i]);
+   printf("entrer les element :\n");
 
-   }
-    printf("entrer un element");
-     scanf("%d", &y);
-     for(int i = 0; i < n; i++){
-      if(T[i]== y){
-          printf("element present");
-          return 0;
-      }
-    }
-     printf("element non present");
+  for(int i =0; i < n; i++){
+      scanf("%d", &T[i]);
 
+  }
+  printf("entre un element :");
+   scanf("%d",&y);
+
+  for(int i =0; i > n; i++){
+  if(T[i] == y){
+      printf("ces element trouver");
+     return 0;
+  }
+}
+
+printf("ce element non trouver\n");
 
 
 
